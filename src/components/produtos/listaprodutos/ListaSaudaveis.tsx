3a242c5e3c3e7be103d0e0ec6cf3a4f6ccda871a
Nathan <ribeiro.nathan2002@gmail.com>
2025-03-06 11:44:07 -0300

    Estilizando lista de produtos saudáveis

--- src/components/produtos/listaprodutos/ListaSaudaveis.tsx
+++ src/components/produtos/listaprodutos/ListaSaudaveis.tsx
@@ -57,7 +57,7 @@ function ListaSaudaveis() {
             <div className="flex justify-center w-full my-4">
                 <div className="container flex flex-col mx-2">
                     <div className="container mx-auto my-4 
-                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"
+                        grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"
                     >
                         {produtos.map((produto) => (
                             <CardProdutos key={produto.id} item={produto} />
--- src/pages/paginaproduto/PaginaSaudaveis.tsx
+++ src/pages/paginaproduto/PaginaSaudaveis.tsx
@@ -5,7 +5,7 @@ function Loja() {
     return (
         <div className="bg-[#f5c840ff] min-h-screen py-8 px-4">
             <div className="container mx-auto">
-                <h1 className="text-2xl font-bold text-black mb-6">Produtos Saudaveis</h1>
+                <h1 className="text-4xl font-bold text-black mb-6">Produtos Saudáveis</h1>
 
                 <ListaSaudaveis />
 
